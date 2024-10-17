86d60e8b617ceef150c57fd20789af28c1d41069
Marcelo Assis <marceloassis502@gmail.com>
2024-10-16 21:45:54 -0300

    Arquivo User em model e instalação de dependencias necessárias

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Método para criptografar a senha antes de salvar o usuário
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Método para comparar senhas no login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
